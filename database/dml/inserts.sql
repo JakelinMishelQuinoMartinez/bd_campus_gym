acc547e322d5061b900571fca85fde1e8c1c130d
JakelinMishelQuinoMartinez <quinojakelin@gmail.com>
2026-08-17 15:20:15 -0600

    feat(dml): insertar datos a la tabla ciudades

---
+++ database/dml/inserts.sql
@@ -0,0 +1,9 @@
+USE campuslands_gym;
+
+-- =================================================== DATOS PARA CIUDADES
+INSERT INTO ciudades (nombre) VALUES 
+('Bogotá'), 
+('Medellín'), 
+('Cali'), 
+('Barranquilla'), 
+('Cartagena');
