d09caf939bb0a968082364fb5007af6d2588d25a
JakelinMishelQuinoMartinez <quinojakelin@gmail.com>
2026-08-17 15:21:02 -0600

    feat(dml): insertar datos a la tabla socios

--- database/dml/inserts.sql
+++ database/dml/inserts.sql
@@ -15,3 +15,16 @@ INSERT INTO sedes (nombre, id_ciudad) VALUES
 ('Sede Centro Medellín', 2), 
 ('Sede Occidente Cali', 3), 
 ('Sede Puerto Barranquilla', 4);
+
+-- =================================================== DATOS PARA SOCIOS
+INSERT INTO socios (nombres, apellidos, telefono) VALUES 
+('Carlos', 'Gómez', '3101234567'),
+('Ana', 'Martínez', '3209876543'),
+('Luis', 'Rodríguez', '3114567890'),
+('María', 'Pérez', '3156789012'),
+('Jorge', 'Díaz', '3123456789'),
+('Sofía', 'López', '3198765432'),
+('Diego', 'Fernández', '3187654321'),
+('Laura', 'Romero', '3176543210'),
+('Andrés', 'Torres', '3165432109'),
+('Valentina', 'Castro', '3143210987');
