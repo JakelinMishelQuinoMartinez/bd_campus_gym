0574ca9f3c8e4fab715eb1028741b95ae5b9cfb5
JakelinMishelQuinoMartinez <quinojakelin@gmail.com>
2026-08-17 15:13:50 -0600

    feat(ddl): crear tabla socios

---
+++ database/ddl/schema.sql
@@ -0,0 +1,11 @@
+-- =================================================== CREACIÓN DE LA BASE DE DATOS
+CREATE DATABASE IF NOT EXISTS campuslands_gym;
+USE campuslands_gym;
+
+-- =================================================== SOCIOS
+CREATE TABLE IF NOT EXISTS socios (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombres VARCHAR(60) NOT NULL,
+    apellidos VARCHAR(60) NOT NULL,
+    telefono VARCHAR(15) NOT NULL
+);
