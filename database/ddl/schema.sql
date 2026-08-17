ef2281d8a4f837e25f3e6a98a06b4f2503784cdd
JakelinMishelQuinoMartinez <quinojakelin@gmail.com>
2026-08-17 15:14:30 -0600

    feat(ddl): crear tabla sedes

--- database/ddl/schema.sql
+++ database/ddl/schema.sql
@@ -15,3 +15,11 @@ CREATE TABLE IF NOT EXISTS ciudades (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR(120) NOT NULL
 );
+
+-- =================================================== SEDES
+CREATE TABLE IF NOT EXISTS sedes (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(120) NOT NULL,
+    id_ciudad INT NOT NULL,
+    FOREIGN KEY (id_ciudad) REFERENCES ciudades(id)
+);
