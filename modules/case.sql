429557f51d98b5112cfbff1a7382c3002f34c000
JakelinMishelQuinoMartinez <quinojakelin@gmail.com>
2026-08-17 15:54:08 -0600

    feat(module): procedimiento con case para asignar plan según tipo de socio

--- modules/case.sql
+++ modules/case.sql
@@ -13,3 +13,30 @@ SELECT
         ELSE 'Edad no determinada'
     END AS rango_edad
 FROM socios;
+
+-- =================================================== CASE EN PROCEDIMIENTO
+-- Procedimiento para asignar plan según tipo de socio
+DELIMITER //
+CREATE PROCEDURE asignar_plan_segun_socio(IN socio_id INT)
+BEGIN
+    DECLARE tipo VARCHAR(20);
+    
+    -- Determinar tipo de socio (ejemplo: basado en cantidad de planes)
+    SELECT 
+        CASE 
+            WHEN COUNT(spe.id) = 0 THEN 'Nuevo'
+            WHEN COUNT(spe.id) = 1 THEN 'Regular'
+            ELSE 'VIP'
+        END INTO tipo
+    FROM socios s
+    LEFT JOIN socio_plan_entrenadores spe ON s.id = spe.socio_id
+    WHERE s.id = socio_id;
+    
+    -- Mostrar resultado
+    SELECT CONCAT('El socio ', socio_id, ' es de tipo: ', tipo) AS mensaje;
+END //
+DELIMITER ;
+
+-- Ejecutar procedimiento
+CALL asignar_plan_segun_socio(1);
+CALL asignar_plan_segun_socio(3);
