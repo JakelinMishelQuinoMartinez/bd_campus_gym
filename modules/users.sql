5d8fd7c0bb9d2eaf92638abf9ff5a8502a61f900
JakelinMishelQuinoMartinez <quinojakelin@gmail.com>
2026-08-17 16:49:57 -0600

    feat(module): asignar permisos y verificar privilegios

--- modules/users.sql
+++ modules/users.sql
@@ -12,3 +12,13 @@ CREATE USER IF NOT EXISTS 'admin_gym'@'localhost' IDENTIFIED BY 'AdminGym2024#';
 
 -- Crear usuario consultor
 CREATE USER IF NOT EXISTS 'consultor_gym'@'localhost' IDENTIFIED BY 'ConsulGym2024$';
+
+-- =================================================== ASIGNAR PERMISOS
+-- Asignar permisos básicos al usuario normal
+GRANT SELECT, INSERT ON campuslands_gym.socios TO 'usuario_gym'@'localhost';
+GRANT SELECT ON campuslands_gym.entrenadores TO 'usuario_gym'@'localhost';
+GRANT SELECT ON campuslands_gym.planes_entrenamientos TO 'usuario_gym'@'localhost';
+GRANT SELECT ON campuslands_gym.sedes TO 'usuario_gym'@'localhost';
+
+-- =================================================== VER PRIVILEGIOS DE UN USUARIO
+SHOW GRANTS FOR 'usuario_gym'@'localhost';
